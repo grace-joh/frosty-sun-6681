304d21daf774e5d66a6493bf06c1bf962febf92b
Grace Joh <graceehjoh@gmail.com>
2023-04-24 10:43:55 -0500

    add seed and test data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,4 +70,54 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+
+  def test_data
+    delete_data
+
+    # hospitals
+    @grey = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+    @seaside = Hospital.create!(name: 'Seaside Health & Wellness Center')
+
+    # doctors
+    @merideth = @grey.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University')
+    @alex = @grey.doctors.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University')
+    @miranda = @seaside.doctors.create!(name: 'Miranda Bailey', specialty: 'General Surgery', university: 'Stanford University')
+    @derek = @seaside.doctors.create!(name: 'Derek McDreamy Shepherd', specialty: 'Attending Surgeon', university: 'University of Pennsylvania')
+
+    # patients
+    @katie = Patient.create!(name: 'Katie Bryce', age: 24)
+    @denny = Patient.create!(name: 'Denny Duquette', age: 39)
+    @rebecca = Patient.create!(name: 'Rebecca Pope', age: 32)
+    @zola = Patient.create!(name: 'Zola Shepherd', age: 2)
+
+    # meredith's patients
+    @doc_pat1 = DoctorPatient.create!(doctor: @merideth, patient: @katie)
+    @doc_pat2 = DoctorPatient.create!(doctor: @merideth, patient: @denny)
+    @doc_pat3 = DoctorPatient.create!(doctor: @merideth, patient: @rebecca)
+    @doc_pat4 = DoctorPatient.create!(doctor: @merideth, patient: @zola)
+
+    # miranda's patients
+    @doc_pat5 = DoctorPatient.create!(doctor: @miranda, patient: @katie)
+    @doc_pat6 = DoctorPatient.create!(doctor: @miranda, patient: @denny)
+    @doc_pat7 = DoctorPatient.create!(doctor: @miranda, patient: @rebecca)
+
+    # alex's patients
+    @doc_pat8 = DoctorPatient.create!(doctor: @alex, patient: @katie)
+    @doc_pat8 = DoctorPatient.create!(doctor: @alex, patient: @rebecca)
+
+    # derek's patients
+    @doc_pat8 = DoctorPatient.create!(doctor: @derek, patient: @katie)
+
+    # katie sees all 4 doctors
+    # denny sees meredith and miranda
+    # rebecca sees meredith, alex, and miranda
+    # zola sees only meredith
+  end
+
+  def delete_data
+    DoctorPatient.delete_all
+    Patient.delete_all
+    Doctor.delete_all
+    Hospital.delete_all
+  end
 end
