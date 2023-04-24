e8fad6318c06795faf08aea77381e3e813e3d632
Grace Joh <graceehjoh@gmail.com>
2023-04-24 12:25:02 -0500

    fix: add user story 1 tests (accidentally deleted)

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -7,6 +7,29 @@ RSpec.describe 'the Doctors show page' do
     visit doctor_path(@miranda)
   end
 
+  describe 'User Story 1' do
+    it 'displays doctors information' do
+      expect(page).to have_content(@miranda.name)
+      expect(page).to have_content(@miranda.specialty)
+      expect(page).to have_content(@miranda.university)
+      expect(page).to have_content(@miranda.hospital_name)
+
+      expect(page).to_not have_content(@meredith.name)
+    end
+
+    it 'displays a list of doctors patients' do
+      within('#patient-list') do
+        expect(page).to have_content('Patients:')
+
+        @miranda.patients.each do |patient|
+          expect(page).to have_content(patient.name)
+        end
+
+        expect(page).to_not have_content(@zola.name)
+      end
+    end
+  end
+
   describe 'User Story 2' do
     it 'displays a delete button next to each patient' do
       @miranda.patients.each do |patient|
