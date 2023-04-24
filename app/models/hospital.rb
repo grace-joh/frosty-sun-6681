8fa6e50f102a83622dae44527e5586b192033a1d
Grace Joh <graceehjoh@gmail.com>
2023-04-24 12:21:45 -0500

    Extension

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,10 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctors_by_most_patients
+    doctors.joins(:patients)
+           .group('doctors.id')
+           .select('doctors.*, COUNT(patients) as patient_count')
+           .order('patient_count DESC')
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many :doctors }
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      test_data
+    end
+
+    describe 'doctors_by_most_patients' do
+      it 'lists all hospitals doctors by most number of patients' do
+        expect(@grey.doctors_by_most_patients).to eq([@meredith, @alex])
+        expect(@seaside.doctors_by_most_patients).to eq([@miranda, @derek])
+      end
+    end
+  end
 end
